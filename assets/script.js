e824ecc00e238246550a6adb098bfbed6e25bf73
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-05-20 17:36:28 -0700

    building HTML framework with styling

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+//store weather API key in variable
+const apiKey = 'da661090c07b3801270d73c03cca3668';
